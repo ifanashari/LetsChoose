2e4b69a06e9e7db7b8e5d3642d1b9f4f2bdaacf2
ifanashari <ifanashari1234@gmail.com>
2018-01-22 19:06:20 +0700

    M Dashboard experiment --> Done

--- src/app/router.ts
+++ src/app/router.ts
@@ -10,17 +10,10 @@ import { BuatComponent } from './buat/buat.component';
 import { BuatAdminComponent } from './buat-admin/buat-admin.component';
 
 export const navRoute:Routes = [
-<<<<<<< HEAD
     {path: '', component: MainComponent},
-    {path: 'dashboard', component: DashboardComponent},
-    {path: 'login' , component: LoginComponent},
-    {path: 'daftar' , component: DaftarComponent}
-=======
-    {path: '' , component: MainComponent},
     {path: 'dashboard', component: DashboardComponent},
     {path: 'login' , component: LoginComponent},
     {path: 'daftar' , component: DaftarComponent},
     {path: 'buat' , component:BuatComponent},
     {path: 'buatAdmin' , component:BuatAdminComponent}
->>>>>>> baa9a293f2d1b17788e382169097999969e61af1
 ]
