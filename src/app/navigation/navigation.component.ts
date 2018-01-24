3d586d225e0f68e40007c21f3c402bc69a83221d
ifanashari <ifanashari1234@gmail.com>
2018-01-24 09:00:26 +0700

    Progressing --> Yet

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
-
+  img = "../../assets/image/header.jpg";
   constructor() { }
 
   ngOnInit() {
